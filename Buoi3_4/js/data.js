58133b05e6a2f170b36d04072cbe8a0dfbaecd51
Âu Ngọc Diệp <91660142+diepAu13122001@users.noreply.github.com>
2024-05-25 15:59:47 +0700

    buoi3

---
+++ Buoi3_4/js/data.js
@@ -0,0 +1,15 @@
+// kieu du lieu JS -----------------------------------------------
+// kieu nguyen thuy:
+/**
+ * string: "abc"
+ * number - integer: 123
+ * number - float: 12.3
+ * null
+ * undefined
+ * symbol
+ */
+// kieu phuc tap:
+/**
+ * object 
+ * array
+ */
